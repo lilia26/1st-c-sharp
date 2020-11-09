fba1842cb39a7d4815b4ab955e7a0138e79a49d7
lilia26 <72409541+lilia26@users.noreply.github.com>
2020-11-09 08:47:53 -0500

    final

--- 4thCSharpProgram/Program.cs
+++ 4thCSharpProgram/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+This code asks the user for their first name
+Then it asks the user to input two numbers
+Then it asks for the user to pick an operation, addition, subtraction, multiplication, division, and power, and performs that operation
+The program gives the answer to the user
+The program loops five times, each time the user inputsa a new set of numbers and a new operation
+After all the loops the program thanks the user and asks them to come back
+ */
+
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace _4thCSharpProgram
@@ -8,10 +17,10 @@ namespace _4thCSharpProgram
 		static void Main(string[] args)
 		{
 			string firstName;
-			float firstNumber;
-			float secondNumber;
+			double firstNumber;
+			double secondNumber;
 			string operation;
-			float answer;
+			double answer;
 
 			Console.WriteLine("Please enter your first name.");
 			firstName = Console.ReadLine();
@@ -24,26 +33,41 @@ namespace _4thCSharpProgram
 				Console.WriteLine("Please enter a second number.");
 				secondNumber = int.Parse(Console.ReadLine());
 
-				Console.WriteLine("Which operation would you like me to perform? Please answer with addition, subtraction, multiplucation, or division.");
+				Console.WriteLine("Which operation would you like me to perform? Please answer with + (addition), - (subtraction), * (multiplication), / (division), or ^ (power).");
 				operation = Console.ReadLine();
 
-				switch (operation)
+				switch (operation.Trim().ToLower())
 				{
+					case "sum":
 					case "addition":
+					case "+":
 						answer = firstNumber + secondNumber;
 						Console.WriteLine("{0}, the sum of {1} and {2} is {3}.", firstName, firstNumber, secondNumber, answer);
 						break;
+					case "difference":
 					case "subtraction":
+					case "-":
 						answer = firstNumber - secondNumber;
 						Console.WriteLine("{0}, the difference of {1} and {2} is {3}.", firstName, firstNumber, secondNumber, answer);
 						break;
-					case "muktiplucation":
+					case "product":
+					case "multiplication":
+					case "x":
+					case "*":
 						answer = firstNumber * secondNumber;
 						Console.WriteLine("{0}, the product of {1} and {2} is {3}.", firstName, firstNumber, secondNumber, answer);
 						break;
+					case "quotient":
 					case "division":
+					case "/":
 						answer = firstNumber / secondNumber;
-						Console.WriteLine("{0}, the quocient of {1} and {2} is {3}.", firstName, firstNumber, secondNumber, answer);
+						Console.WriteLine("{0}, the quotient of {1} and {2} is {3}.", firstName, firstNumber, secondNumber, answer);
+						break;
+					case "power":
+					case "exponent":
+					case "^":
+						answer = Math.Pow(firstNumber, secondNumber);
+						Console.WriteLine("{0}, {1} to the power of {2} is {3}.", firstName, firstNumber, secondNumber, answer);
 						break;
 					default:
 						Console.WriteLine("Sorry, I didn't understand your input.");
@@ -52,6 +76,8 @@ namespace _4thCSharpProgram
 				Console.ReadLine();
 				Console.Clear();
 			}
+			Console.WriteLine("Thank you for using my basic calculator {0}. Come back soon!", firstName);
+			Console.ReadLine();
 		}
 	}
 }
